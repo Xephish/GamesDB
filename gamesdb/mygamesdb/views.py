95d4e712dea852b7b1c1bf75602e28e557fec309
Lluis <hykrenx@gmail.com>
2016-04-03 17:21:59 +0200

    Update views.py

--- gamesdb/mygamesdb/views.py
+++ gamesdb/mygamesdb/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from	django.core.urlresolvers	import	reverse
+from	django.http	import	HttpResponseRedirect
+from	django.shortcuts	import	get_object_or_404
+from	django.views.generic	import	DetailView
+from	django.views.generic.edit	import	CreateView
+from	models	import	Developers
 
-# Create your views here.
+class	RestaurantDetail(DetailView):
+				model	=	Developer
+				template_name	=	'mygamesdb/developers.html'
